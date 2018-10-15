932aa3e30ac67c55023f3ae8012eaba77b3e621f
TimeWz667 <TimeWz667@gmail.com>
2018-10-15 18:42:57 +0100

    Use remote data

--- javascripts/vis.js
+++ javascripts/vis.js
@@ -54,10 +54,10 @@ function cutBlocks(blocks, stages, end) {
 
 
 d3.queue()
-    .defer(d3.json, "data/output/Pathways.json")
-    .defer(d3.json, "data/Input/Stages_TB.json")
-    // .defer(d3.json, "https://patientpathwayanalysis.github.io/IPPA-data/output/Pathways.json")
-    // .defer(d3.json, "https://patientpathwayanalysis.github.io/IPPA-data/Input/Stages_TB.json")
+    // .defer(d3.json, "data/output/Pathways.json")
+    // .defer(d3.json, "data/Input/Stages_TB.json")
+    .defer(d3.json, "https://patientpathwayanalysis.github.io/IPPA-data/output/Pathways.json")
+    .defer(d3.json, "https://patientpathwayanalysis.github.io/IPPA-data/Input/Stages_TB.json")
     .await(function(error, pathways, stage_maps) {
         console.log(stage_maps);
         console.log(pathways);
